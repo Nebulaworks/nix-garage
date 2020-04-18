a9203de2cfdb83ebb9b6b0b5c53627efe70b6dd8
Robert James Hernandez <rob@sarcasticadmin.com>
2020-04-17 19:48:25 -0700

    Adding labels to awsutils image

--- imgs/awsutils/default.nix
+++ imgs/awsutils/default.nix
@@ -1,14 +1,16 @@
 { system ? builtins.currentSystem }:
 
 let
+  nwi = import ../../nwi.nix;
   pkgs = import ../../pin { snapshot = "nixos-20-03_0"; };
-
-in pkgs.dockerTools.buildImage {
-  name = "nebulaworks/awsutils";
-  # Doesnt matter since we will use the derivation
-  # when publishing to image registry
-  tag = "latest";
+  lib = pkgs.lib;
   contents = [ pkgs.coreutils pkgs.bash pkgs.jq pkgs.curl pkgs.awscli ];
+in pkgs.dockerTools.buildImage {
+  inherit contents;
+  name = "nebulaworks/awsutils";
+  # Doesnt matter will use the derivation
+  # when publishing to registry
+  tag = "latest";
   extraCommands = ''
       # make sure /tmp exists
       mkdir -m 1777 tmp
@@ -17,6 +19,11 @@ in pkgs.dockerTools.buildImage {
     Env = [ 
       "PATH=/bin/"
     ];
+    Labels = {
+      "com.nebulaworks.packages" = lib.strings.concatStringsSep "," (lib.lists.naturalSort (lib.lists.forEach contents (x: lib.strings.getName x + ":" + lib.strings.getVersion x )));
+      "org.opencontainers.image.authors" = nwi.company;
+      "org.opencontainers.image.source" = nwi.source;
+    }; 
     WorkingDir = "/";
   };
 }
---
+++ nwi.nix
@@ -0,0 +1,5 @@
+{
+  company = "Nebulaworks Inc.";
+  homepage = "https://nebulaworks.com/";
+  source = "https://github.com/Nebulaworks/nix-garage";
+}
