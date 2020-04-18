4323ddc31fb5aa5538e4d5de0bb31de70d585498
Robert James Hernandez <rob@sarcasticadmin.com>
2020-04-17 19:50:17 -0700

    Trying out contents inheretance

--- imgs/awsutils/default.nix
+++ imgs/awsutils/default.nix
@@ -3,13 +3,13 @@
 let
   pkgs = import ../../pin { snapshot = "nixos-20-03_0"; };
   lib = pkgs.lib;
-  mypkgs = [ pkgs.coreutils pkgs.bash pkgs.jq pkgs.curl pkgs.awscli ];
+  contents = [ pkgs.coreutils pkgs.bash pkgs.jq pkgs.curl pkgs.awscli ];
 in pkgs.dockerTools.buildImage {
+  inherit contents;
   name = "nebulaworks/awsutils";
   # Doesnt matter will use the derivation
   # when publishing to registry
   tag = "latest";
-  contents = mypkgs;
   extraCommands = ''
       # make sure /tmp exists
       mkdir -m 1777 tmp
@@ -18,7 +18,7 @@ in pkgs.dockerTools.buildImage {
     Env = [ 
       "PATH=/bin/"
     ];
-    Label = { "com.nebulaworks.packages" = lib.strings.concatStringsSep "," (lib.lists.naturalSort (lib.lists.forEach mypkgs (x: lib.strings.getName x + ":" + lib.strings.getVersion x )));
+    Labels = { "com.nebulaworks.packages" = lib.strings.concatStringsSep "," (lib.lists.naturalSort (lib.lists.forEach contents (x: lib.strings.getName x + ":" + lib.strings.getVersion x )));
     }; 
     WorkingDir = "/";
   };
