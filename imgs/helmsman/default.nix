ec0ea6a0ca32a670753cdfdee426d2c2bb7e9f81
Lynn Dong <ldong@nebulaworks.com>
2020-05-13 20:29:01 -0700

    Adding Helmsman container image

---
+++ imgs/helmsman/default.nix
@@ -0,0 +1,29 @@
+{ system ? builtins.currentSystem }:
+
+let
+  nwi = import ../../nwi.nix;
+  pkgs = import ../../pin { snapshot = "nixos-unstable_0"; };
+  lib = pkgs.lib;
+  contents = [ pkgs.bash pkgs.helmsman pkgs.jq pkgs.awscli ];
+in pkgs.dockerTools.buildImage {
+  inherit contents;
+  name = "nebulaworks/helmsman-aws";
+  # Doesnt matter will use the derivation
+  # when publishing to registry
+  tag = "latest";
+  extraCommands = ''
+      # make sure /tmp exists
+      mkdir -m 1777 tmp
+  '';
+  config = {
+    Env = [ 
+      "PATH=/bin/"
+    ];
+    Labels = {
+      "com.nebulaworks.packages" = lib.strings.concatStringsSep "," (lib.lists.naturalSort (lib.lists.forEach contents (x: lib.strings.getName x + ":" + lib.strings.getVersion x )));
+      "org.opencontainers.image.authors" = nwi.company;
+      "org.opencontainers.image.source" = nwi.source;
+    }; 
+    WorkingDir = "/";
+  };
+}
