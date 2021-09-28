3df9d4a10832cb6e4f99438ae0a0c33a945edad6
Robert James Hernandez <rob@sarcasticadmin.com>
2021-09-27 18:06:14 -0700

    Make overlay alphabetical

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,9 @@
 self: super:
 {
+  aws-key-rotator = super.callPackage ./pkgs/aws-key-rotator { };
+  codefresh = super.callPackage ./pkgs/codefresh { };
   flasksample = super.callPackage ./pkgs/flasksample { };
   git-divergence = super.callPackage ./pkgs/git-divergence { };
   sshcb = super.callPackage ./pkgs/sshcb { };
-  codefresh = super.callPackage ./pkgs/codefresh { };
-  aws-key-rotator = super.callPackage ./pkgs/aws-key-rotator { };
   terraform-config-inspect = super.callPackage ./pkgs/terraform-config-inspect { };
 }
